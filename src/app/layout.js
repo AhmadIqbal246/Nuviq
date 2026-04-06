487f23529d518ef89227bdd0d82fdf15abdfb109
AhmadIqbal246 <160164019+AhmadIqbal246@users.noreply.github.com>
2026-04-06 22:57:04 +0500

    Add script tag for widget API in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
         <ClientLayout>
           {children}
         </ClientLayout>
+  <script src="http://localhost:3000/api/widget"></script>
       </body>
     </html>
   );
